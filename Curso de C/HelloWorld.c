983c3a739022bb6152a18fc886a23891b5fb0d66
Jaime Somohano <jaimesdejesus10@hotmail.com>
2022-02-01 17:08:42 -0600

    Creando mi primer Hello World en C

---
+++ Curso de C/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    /*Este es mi primer mensaje
+    de Hello World en c
+    Mientras estoy en Visual
+    Studio code
+    */
+
+    printf("Hello World");
+}
